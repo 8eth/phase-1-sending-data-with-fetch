a05fc79538d99b61c572adf2012213a515a1a524
Betelihem <Betelihem>
2022-02-10 21:56:25 -0800

    commit tree 7eb87429e5a769d0425f17d408ed8ae2f3b7c8bd

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+const body = document.querySelector('body')
+
+function submitData (name, email) {
+    return fetch ('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+
+    .then (res => res.json())
+
+    .then (function(userObj) {
+        const div = document.createElement('div')
+        div.textContent = userObj.id
+        body.appendChild(div)
+    })
+    
+    .catch (function() {
+        const li = document.createElement('li')
+        li.textContent = 'Unauthorized Access'
+        body.appendChild(li)
+    })
+}
